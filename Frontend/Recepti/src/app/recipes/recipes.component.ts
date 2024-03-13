5455840a82ebe6d090cc58cdaeb015cfcfdea72c
DESKTOP-3K51J54\Kristina <kristina.anicic1@gmail.com>
2024-03-14 00:51:39 +0100

    added initial recipe page

---
+++ Frontend/Recepti/src/app/recipes/recipes.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-recipes',
+  templateUrl: './recipes.component.html',
+  styleUrls: ['./recipes.component.css']
+})
+export class RecipesComponent {
+
+  constructor(private router: Router) { }
+
+  navigateToHome() {
+    this.router.navigate(['/']);
+  }
+}
