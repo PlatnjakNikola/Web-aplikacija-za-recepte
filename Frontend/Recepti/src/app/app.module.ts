54280a062f7709ceb39eb5eb41037233edf66ad6
kristinaa14 <kristina.anicic1@gmail.com>
2024-03-19 00:25:14 +0100

    added firebase for uploading images

--- Frontend/Recepti/src/app/app.module.ts
+++ Frontend/Recepti/src/app/app.module.ts
@@ -1,15 +1,10 @@
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { importProvidersFrom, NgModule } from '@angular/core';
+import { NgModule, importProvidersFrom } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-/*import { AngularFireModule } from '@angular/fire';
-import { AngularFireStorageModule } from '@angular/fire/storage';
-import { environment } from '../environments/environment';*/
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideStorage, getStorage } from '@angular/fire/storage';
 
-import { AngularFireModule } from '@angular/fire/compat';
-import { AngularFireStorageModule } from '@angular/fire/compat/storage';
 
 import { AppComponent } from './app.component';
 import { RecipesComponent } from './recipes/recipes.component';
@@ -19,7 +14,6 @@ import { AppRoutingModule, RoutingModule } from './app-routing.module';
 import { LoginRegisterComponent } from './account/login-register/login-register.component';
 import { AddEditRecipeComponent } from './recipes/add-edit-recipe/add-edit-recipe.component';
 
-
 const firebaseConfig = {
   apiKey: "AIzaSyAQykz8OsI4oB8IRd4tE8XoRjLR0hnDBfE",
   authDomain: "web-aplikacija-za-recept-c7e86.firebaseapp.com",
@@ -28,6 +22,7 @@ const firebaseConfig = {
   messagingSenderId: "856621045537",
   appId: "1:856621045537:web:6080fd981b5d0313599060"
 };
+
 
 @NgModule({
   declarations: [
@@ -44,10 +39,11 @@ const firebaseConfig = {
     FormsModule,
     ReactiveFormsModule,
     AppRoutingModule,
-    AngularFireModule.initializeApp(firebaseConfig),
-    AngularFireStorageModule
   ],
-  providers: [],
+  providers: [importProvidersFrom([
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideStorage(() => getStorage())
+  ])],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- Frontend/Recepti/src/app/recipes/add-edit-recipe/add-edit-recipe.component.ts
+++ Frontend/Recepti/src/app/recipes/add-edit-recipe/add-edit-recipe.component.ts
@@ -2,8 +2,10 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ActivatedRoute, Router } from '@angular/router';
 import { RecipesService, Recipe } from 'src/app/services/recipes.service';
-//import { AngularFireModule } from '@angular/fire/compat';
-import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { FirebaseStorage } from '@angular/fire/storage';
+
+import { doc, docData, Firestore, setDoc, getDoc, } from '@angular/fire/firestore';
+import { getDownloadURL, ref, Storage, uploadString, uploadBytes, getStorage } from '@angular/fire/storage';
 
 @Component({
   selector: 'app-add-edit-recipe',
@@ -11,81 +13,47 @@ import { AngularFireStorage } from '@angular/fire/compat/storage';
   styleUrls: ['./add-edit-recipe.component.css']
 })
 export class AddEditRecipeComponent implements OnInit {
-  recipe!: Recipe;
+  recipe!: any;
+  id!: string;
+  file!: File;
   activateAddEditIngredient: boolean = false;
   ingredientbackUp!: string;
   ingreditnt!: string;
   recipeTypes:String[] = ["Breakfast", "Lunch", "Dinner", "Appetizer", "Salad", "Main-course", "Side-dish", "Baked-goods", "Dessert", "Snack", "Soup", "Holiday", "Vegetarian"];
-  constructor(private fireStorage: AngularFireStorage) { }
+  constructor(private storage: Storage) { }
 
-  @Input() recipeEdit!: Recipe;
+  @Input() recipeEdit!: any;
 
   ngOnInit() {
-    //this.recipe = this.recipeEdit;
     this.recipe = { ...this.recipeEdit };
+    this.id = this.recipeEdit.id;
+  }
+  url = `../../assets/img/image${this.id}.png`;
+
+  onSelectFile(event: any) {
+    this.file = event.target.files[0];
   }
 
-  async onFileChange(event: any) {
-    const file: File = event.target.files[0];
-    if (file) {
-      const path = `yt/${file.name}`;
-      const uploadTask = await this.fireStorage.upload(path, file);
-      const url = await uploadTask.ref.getDownloadURL();
-      console.log(url);
+  async uploadFile(file: File) {
+    const storageRef = ref(getStorage(), `image${this.id}`);
+    const uploadTask = uploadBytes(storageRef, file);
+
+    try {
+      const snapshot = await uploadTask;
+      console.log('Image uploaded successfully!');
+      const downloadURL = await getDownloadURL(snapshot.ref);
+      console.log('Download URL:', downloadURL);
+      // Save the URL to your database or use it as needed
+    } catch (error) {
+      console.error('Error uploading file:', error);
     }
   }
 
-  /*handleFileInput(event: any) {
-    const file: File = event.target.files[0];
-    if (file) {
-      
+  saveChanges() {
+    if (this.file) {
+      this.uploadFile(this.file);
     }
   }
 
-  onFileSelected(event: any) {
-    const file: File = event.target.files[0];
-    this.saveFileToLocalAssets(file);
-  }
-
-  saveFileToLocalAssets(file: File) {
-    const reader = new FileReader();
-    reader.onload = (event: any) => {
-      const base64Image: string = event.target.result;
-      const img = new Image();
-      img.src = base64Image;
-
-      // Pretpostavljamo da ćemo sliku spremiti u assets/images folderu
-      const filePath = `../../assets/images/${file.name}`;
-
-      // Stvaranje slike na canvasu i spremanje u assets folder
-      const canvas = document.createElement('canvas');
-      const ctx = canvas.getContext('2d');
-      canvas.width = img.width;
-      canvas.height = img.height;
-      if (ctx !== null) {
-        ctx.drawImage(img, 0, 0);
-      }
-      else {
-        console.error("Canvas context is null.");
-      }
-      const imageData = canvas.toDataURL('image/jpeg', 1.0);
-
-      // Pretvaranje base64 slike u File objekt
-      const byteCharacters = atob(imageData.split(',')[1]);
-      const byteNumbers = new Array(byteCharacters.length);
-      for (let i = 0; i < byteCharacters.length; i++) {
-        byteNumbers[i] = byteCharacters.charCodeAt(i);
-      }
-      const byteArray = new Uint8Array(byteNumbers);
-      const imageFile = new File([byteArray], file.name, { type: 'image/jpeg' });
-
-      // Spremanje slike u lokalni assets folder
-      const a = document.createElement('a');
-      a.href = imageData;
-      a.download = file.name;
-      a.click();
-    };
-
-    reader.readAsDataURL(file);
-  }*/
+  
 }
