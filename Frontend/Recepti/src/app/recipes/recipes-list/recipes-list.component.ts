9b3b20d8d94d3f874ed85f17ccbb11f9298fab24
kristinaa14 <kristina.anicic1@gmail.com>
2024-03-26 14:52:04 +0100

    added "add recipe" button and trash can for deleting recipes

--- Frontend/Recepti/src/app/recipes/recipes-list/recipes-list.component.ts
+++ Frontend/Recepti/src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { RecipesService } from 'src/app/services/recipes.service';
+import { Recipe, RecipesService } from 'src/app/services/recipes.service';
 import { Router } from '@angular/router';
+import { share } from 'rxjs/operators';
 
 @Component({
   selector: 'app-recipes-list',
@@ -13,18 +14,21 @@ export class RecipesListComponent implements OnInit {
   recipesList$!: Observable<any[]>;
   recipesListNoFilter$!: Observable<any[]>;
   recipes: any[] = [];
+  admin: boolean = false;
+  add: boolean = false;
 
   //Filtering
   recipesFilter: any = [];
-  MealFilter: string = '';
-  typeFilter: string = '';
+  search: string = '';
+  //typeFilter: string = '';
   filtered: boolean = false;
-  recipe: any;
+  recipe!: Recipe;
 
   constructor(private service: RecipesService, private http: HttpClient, private router: Router) {}
 
    ngOnInit() : void{
      this.recipesList$ = this.service.getRecipesList();
+     this.recipesListNoFilter$ = this.service.getRecipesList();
    //this.recipesList$ = this.http.get<any>("../../../../recepti.json");
    //this.recipesList$ = this.http.get<any>("..\\..\\..\\assets\\recepti.json");
     }
@@ -32,5 +36,38 @@ export class RecipesListComponent implements OnInit {
    recipeShow(item: any) {
      this.router.navigate(['/recipe'], { queryParams: { recipe: JSON.stringify(item) } });
      //this.router.navigate(['/recipe', item.id]);
+  }
+
+  modalClose() {
+    this.recipesList$ = this.service.getRecipesList();
+  }
+  addRecipe() {
+    this.add = true;
+    this.recipe = {
+      id: 0,
+      title: "",
+      ingredients:[],
+      description: "",
+      timeToPrepare: 0,
+      type: "",
+      image: "",
+      enabled: false
+    }
+  }
+
+  deleteRecipe(recipe:any) {
+    this.service.deleteRecipe(recipe.id).subscribe(() => {
+      this.recipesList$ = this.service.getRecipesList();
+    },
+      (error: any) => {
+        alert(error.error);
+      });
+
+  }
+
+  searchRecipe() {
+    this.recipesListNoFilter$ = this.recipesList$.pipe(share());
+    console.log("Search:" + this.search);
+    this.recipesList$ = this.service.getFilteredRecipesList(this.search);
   }
 }
