1f2fec08e477a6fd44aba5cd7f388dc0a2f60844
kristinaa14 <kristina.anicic1@gmail.com>
2024-04-02 02:52:26 +0200

    added authorization service for remembering login

---
+++ Frontend/Recepti/src/app/services/auth.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private loggedIn = false;
+  private user: any = null;
+
+  constructor() {
+    this.loggedIn = localStorage.getItem('loggedIn') === 'true';
+    this.user = JSON.parse(localStorage.getItem('user') || '{}');
+  }
+
+  isLoggedIn(): boolean {
+    return this.loggedIn;
+  }
+
+  getUserData(): any {
+    return JSON.parse(localStorage.getItem('user') || '{}');
+  }
+
+  login(userData: any) {
+    localStorage.setItem('loggedIn', 'true');
+    localStorage.setItem('user', JSON.stringify(userData));
+    this.loggedIn = true;
+    this.user = userData;
+  }
+
+  logout() {
+    this.loggedIn = false;
+    localStorage.setItem('loggedIn', 'false');
+    localStorage.removeItem('user');
+    this.loggedIn = false;
+    this.user = null;
+  }
+}
